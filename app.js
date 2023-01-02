284fdd507497aa26a3a6b71aea18ac33453a2755
Sercan Coskun <sercancoskun001@gmail.com>
2023-01-01 22:18:29 -0600

    Update app.js
    UPDATED PORT constant

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
+const PORT = process.env.PORT || 3003;
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -34,6 +35,6 @@ app.post("/", function(req,res){
     
 });
 
-app.listen(3002, function(){
-    console.log("Listening port 3002.");
+app.listen(PORT, function(){
+    console.log(`Listening port ${PORT}.`);
 });
